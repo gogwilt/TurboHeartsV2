e048a3d804142e94a600272122bd60b277f68611
Cai GoGwilt <gogwilt@gmail.com>
2011-12-30 11:01:21 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turbohearts::Application.config.secret_token = '7b84f953c27f9b2cbbdcd7362478533e81c35a6ee13c59b3b6f7286c94c02c4a845fbb305e25e517db3b337b43084a9d6c2b92d93aea43334f96601f61b97601'
