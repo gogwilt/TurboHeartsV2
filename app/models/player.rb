6bb622b43bc5c3816c49b77369d7f070212e4cba
Cai GoGwilt <gogwilt@gmail.com>
2011-12-31 10:17:40 -0800

    Adds validations for player and point fields.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ActiveRecord::Base
   has_many :points
   has_many :rounds, :through => :points
   
+  validates_presence_of :name
+  
   def winnings
     self.points.inject(0) {|cumulative, point| cumulative + point.dollar_value}
   end
--- app/models/point.rb
+++ app/models/point.rb
@@ -2,6 +2,10 @@ class Point < ActiveRecord::Base
   belongs_to :round
   belongs_to :player
   
+  validates_presence_of :player
+  validates_presence_of :round
+  validates_presence_of :value
+  
   def dollar_value
     total_points = self.round.points.inject(0) {|cumulative, point| cumulative + point.value}
     total_points - self.value * 5
