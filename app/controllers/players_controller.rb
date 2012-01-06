dc973c62a0cf2f260f96742945665cdb835adcd1
Cai GoGwilt <gogwilt@gmail.com>
2012-01-06 13:35:03 -0800

    Search results look better, and don't have any damaging actions.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -84,13 +84,4 @@ class PlayersController < ApplicationController
     end
   end
   
-  private
-  def get_a_league_for_player(player)
-    League.all.each do |l|
-      if l.get_players.include? player
-        return l
-      end
-    end
-    return nil    
-  end
 end
--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -1,2 +1,10 @@
 module LeaguesHelper
+  def get_a_league_for_player(player)
+    League.all.each do |l|
+      if l.get_players.include? player
+        return l
+      end
+    end
+    return nil    
+  end
 end
