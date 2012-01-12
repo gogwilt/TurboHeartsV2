bf0c006e913838aee81c5d21719a15d3c3ef7291
Cai GoGwilt <gogwilt@gmail.com>
2012-01-11 20:52:14 -0800

    Redirects to dashboard from home if user is logged in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def home
     @do_not_show_banner = true
+    if current_user
+      redirect_to dashboard_path
+    end
   end
 
   def learn_turbohearts
